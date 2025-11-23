178d281abcd37927a2ff41f360b9e20f50cd652f
Hariom Singh <suppraaaa2003@gmail.com>
2025-11-24 01:32:36 +0530

    define distionaly

---
+++ pythonDictionaryes.py
@@ -0,0 +1,4 @@
+# A Python dictionary is a collection of items, similar to
+# lists and tuples. However, unlike lists and tuples,
+# each item in a dictionary is a key-value pair
+# (consisting of a key and a value).
