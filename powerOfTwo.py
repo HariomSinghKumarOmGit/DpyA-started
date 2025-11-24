ddad0f0ccc469b95c57f52887209c05cf741d13a
Hariom Singh <suppraaaa2003@gmail.com>
2025-11-25 04:43:47 +0530

    recourson and while

--- powerOfTwo.py
+++ powerOfTwo.py
@@ -1,17 +1,32 @@
 # by while loop 
 n = 150
 
+# class Solution:
+#     def isPowerOfTwo(self, n: int) -> bool:
+#         if n<=0:
+#             return False
+#         while n%2==0 :
+#             n //= 2
+ 
+#         print("hello")
+#         return n ==1
+    
+
+
+#   RECOURSION =========> 
+
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
-        if n<=0:
+        # base case
+        if (n<=0):
             return False
-        while n%2==0 :
-            n //= 2
- 
-        print("hello")
-        return n ==1
-    
-    
+        if n==1:
+            return True
+        if n%2!=0:
+            return False
+        # recursive case
+        return self.isPowerOfTwo(n//2)
+
 
 
 
