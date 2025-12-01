7575cee1b47bd60d1ed88f0bb0f0518e66efcd25
Hariom Singh <suppraaaa2003@gmail.com>
2025-12-02 02:20:37 +0530

    upper bound is not equal its always grater

---
+++ upperBound.py
@@ -0,0 +1,22 @@
+# lower or upper bound
+
+class Solution:
+    def lbound(self, nums, target):
+        n = len(nums)
+        l=0
+        r=n-1
+        ans=n
+        while l<=r:
+            mid = (l+r)//2
+
+            if nums[mid]> target:
+                ans=mid
+                r = mid-1
+            else:
+                # right
+                l=mid+1
+        return ans
+
+
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return self.lbound(nums, target)
