ff92977da8d2abae577253fe2c3ff4824ca0681b
Hariom Singh <suppraaaa2003@gmail.com>
2025-12-01 23:49:20 +0530

    boombam

--- 704Binerry.py
+++ 704Binerry.py
@@ -1,19 +1,3 @@
 nums = [2, 5, 9, 30, 45, 62, 10 ,5]
 target = 10
 
-
-class Solution:
-    def search(nums, target):
-        n = leng(nums)
-
-        l = 0
-        r = n-1
-
-        while l<=r:
-            if target == nums[mid]:
-                return mid
-            elif target > nums[mid]:
-                l = mid+1
-            else:
-                r = mid-1
-        return -1
