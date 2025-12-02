92b64913111fb31a4b42428623a18864dc5cfc89
Hariom Singh <suppraaaa2003@gmail.com>
2025-12-03 02:12:38 +0530

    fixed the len prob

---
+++ searchA2dMatrix.py
@@ -0,0 +1,19 @@
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        row = len(matrix)
+        col = len(matrix[0])
+
+        l = 0
+        r = row*col -1
+
+        while r>=l:
+            mid = (l+r)//2
+            if matrix[mid//col][mid%col] == target:
+                return True
+            elif matrix[mid//col][mid%col] > target:
+                # reft
+                r = mid -1
+            else:
+                # right
+                l = mid +1
+        return False
