8075e9f2451728c482c17beaa2fe632a39e91443
Hariom Singh <suppraaaa2003@gmail.com>
2025-12-03 18:00:15 +0530

    learned node to insert at start, insert at end and inseret in mid

--- Section-5/LinkedList2eg.py
+++ Section-5/LinkedList2eg.py
@@ -19,4 +19,25 @@ a.next = b
 b.next = c
 head = a
 
+# insert at start
+newNode = Node(4)
+newNode.next = head
+head = newNode
+
+# insert at end
+newNode = Node(11)
+curr = head
+while curr.next!=None:
+    curr = curr.next
+curr.next = newNode
+
+# insert at kth index
+newNode = Node(7)
+k = 4
+curr = head
+for i in range(k-2):
+    curr = curr.next
+newNode.next = curr.next
+curr.next = newNode
+
 printLL(head)
