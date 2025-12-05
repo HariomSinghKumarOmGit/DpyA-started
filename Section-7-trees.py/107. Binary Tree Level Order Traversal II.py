1bf814a191161530039782b9de1e801cfea26ee1
Hariom Singh <suppraaaa2003@gmail.com>
2025-12-06 05:19:22 +0530

    this is the reversal for based on last q 102 this is q on o107

---
+++ Section-7-trees.py/107. Binary Tree Level Order Traversal II.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+from collections import deque
+
+class Solution:
+    def levelOrderBottom(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if root is None:
+            return []
+
+        q = deque([root])
+        ans = []
+
+        while q:
+            level = []
+            sz = len(q)
+
+            for _ in range(sz):
+                node = q.popleft()
+                level.append(node.val)
+
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+
+            ans.append(level)
+
+        return ans[::-1]
