960101110d980c0fad2a027f832b6a9223294d92
Hariom Singh <suppraaaa2003@gmail.com>
2025-12-07 17:45:24 +0530

    graph eg

---
+++ Graph/ex.py
@@ -0,0 +1,12 @@
+v = 5
+edges = [[0,1],[0,3],[2,3],[3,4], [1,4]]
+
+def buildAdjList(v, edges):
+    adjList = [[]]*v
+
+    for u,v in edges:
+        adjList[u].append(v)
+        adjList[v].append(u)
+    return adjList
+
+print(buildAdjList(v, edges))
