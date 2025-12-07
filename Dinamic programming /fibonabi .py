006e0422927de2b54d952c73ca1cc42e7bb3e0d3
Hariom Singh <suppraaaa2003@gmail.com>
2025-12-07 19:02:37 +0530

    q no 509 solved by recursion

--- Dinamic programming /fibonabi .py
+++ Dinamic programming /fibonabi .py
@@ -1,3 +1,17 @@
 # it the same 1 but with diff approcch time comblacety 
 # befor logn^2 now log n
-hello
+# 509. Fibonacci Number
+class Solution:
+    def rec(self, n, dp):
+        # base case
+        if n<=1:
+            return n
+        # recursive case
+        if dp[n]!=-1:
+            return dp[n]
+        dp[n] = self.rec(n-1, dp) + self.rec(n-2, dp)
+        return dp[n]
+
+    def fib(self, n: int) -> int:
+        dp = [-1]*(n+1)
+        return self.rec(n, dp)
